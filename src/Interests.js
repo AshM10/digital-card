51df0bdbdcf665fe05bcdee043e58e863cb3b9c8
Ash Moreno <ashterfuentes@gmail.com>
2023-01-11 13:08:57 -0600

    feat: add footer

--- src/Interests.js
+++ src/Interests.js
@@ -5,8 +5,8 @@ export default function Interests() {
     <div className="Interests">
       <h2>Interests</h2>
       <p>
-        Food expert. Music scholar. Reader. Internet fanatic. Bacon buff.
-        Entrepreneur. Travel geek. Pop culture ninja. Coffee fanatic.
+        Artist. Musician. Reader. Internet fanatic. Donut buff. Travel geek. Pop
+        culture ninja. Coffee fanatic.
       </p>
     </div>
   );
